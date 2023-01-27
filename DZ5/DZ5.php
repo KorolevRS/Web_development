99da12bffe00c72d36377f31aa0cfda023552cab
Roman <119437468+KorolevRS@users.noreply.github.com>
2023-01-27 22:15:25 +0300

    Add files via upload

---
+++ DZ5/DZ5.php
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<user>
+	<name>"Иван"</name>
+	<surname>"Иванов"</surname>
+	<patronymic>"Иванович"</patronymic>
+	<age>"30"</age>
+	<phone>"Москва"</phone>
+	<birthplace>"+7 926 766 48 48"</birthplace>
+</user>
+
+{
+    "name": "Иван",
+    "surname": "Иванов",
+    "patronymic": "Иванович",
+    "age": "30",
+    "birthplace": "Москва",
+    "phone": "+7 926 766 48 48"
+}
